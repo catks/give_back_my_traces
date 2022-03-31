626c5469e1bf8dcc91ed5584127e3888b1d23dd4
Carlos Atkinson <carlos.atks@gmail.com>
2022-03-31 20:48:35 -0300

    Initial Commit

---
+++ lib/give_back_my_traces.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "give_back_my_traces/version"
+
+module GiveBackMyTraces
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/give_back_my_traces_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe GiveBackMyTraces do
+  it "has a version number" do
+    expect(GiveBackMyTraces::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
